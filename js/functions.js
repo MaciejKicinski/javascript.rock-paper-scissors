515e1324d31cd1e80712ca1bd0d67b8d1984561e
maciej.kicinski <maciej.kicinski@unit4.com>
2023-01-11 20:55:21 +0100

    -copy code form chapter 4.3

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,7 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
